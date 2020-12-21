8f454ce69f4def1238d6730269539d36b7bdba2c
Fisnik Shabani <fisnik.shabani@gmail.com>
2020-12-21 01:11:32 +0100

    Fixed Vet Repository Interface

--- pet-clinic-data/src/main/java/guru/springframework/repositories/VetRepository.java
+++ pet-clinic-data/src/main/java/guru/springframework/repositories/VetRepository.java
@@ -1,7 +1,7 @@
 package guru.springframework.repositories;
 
-import guru.springframework.sfgpetclinic.model.Pet;
+import guru.springframework.sfgpetclinic.model.Vet;
 import org.springframework.data.repository.CrudRepository;
 
-public interface VetRepository extends CrudRepository<Pet, Long> {
+public interface VetRepository extends CrudRepository<Vet, Long> {
 }
